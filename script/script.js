b7c69c006c70f1724d253188159ac151779a6521
mr.z <mr.z004@yahoo.com>
2022-01-07 22:13:22 +0530

    made the navigation bar responsive for mobiles

---
+++ script/script.js
@@ -0,0 +1,34 @@
+// alert("hello world")
+// console.log("hello world")
+// console.warn("hello world")
+// console.error("hello world")
+// console.assert(false, "hello world")
+// console.info("hello world")
+// let a = document.getElementById("webname")
+// console.log(a)
+// document.getElementById("webname").innerHTML =`<div class="webname">\n\t<h1>\n\t\thello\n\t</h1>\n</div>`
+
+// document.getElementById("webname::after").addEventListener("click", togglehide)
+// function togglehide(){
+//     let navigationList = document.getElementById("navdiv");
+//     navigationList.style.display = "none";
+// }
+
+if (window.innerWidth < 768) {
+  ToggleHide();
+}
+
+function ToggleHide() {
+    let searchbar = document.getElementById("navdiv");
+    let aro = document.getElementById("aro");
+
+    if (searchbar.style.display == "none") {
+      searchbar.style.display = "";
+      aro.innerHTML = "ˆ";
+    } else {
+        searchbar.style.display = "none";
+        aro.innerHTML = "ˇ";
+    }
+  }
+
+  // ToggleHide();
