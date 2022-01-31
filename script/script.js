eb4da124c5f26700f892674c6f866916e87543cb
mr.z <mr.z004@yahoo.com>
2022-01-31 13:56:53 +0530

    made the site more responsive

--- script/script.js
+++ script/script.js
@@ -1,45 +1,75 @@
-// alert("hello world")
-// console.log("hello world")
-// console.warn("hello world")
-// console.error("hello world")
-// console.assert(false, "hello world")
-// console.info("hello world")
-// let a = document.getElementById("webname")
-// console.log(a)
-// document.getElementById("webname").innerHTML =`<div class="webname">\n\t<h1>\n\t\thello\n\t</h1>\n</div>`
+if (window.innerWidth < 769) {
+  aro.innerHTML = "ˇ";
 
-// document.getElementById("webname::after").addEventListener("click", togglehide)
-// function togglehide(){
-//     let navigationList = document.getElementById("navdiv");
-//     navigationList.style.display = "none";
+  document.getElementById("resNav").innerHTML = `
+  <ul id="navbar">
+          <li>
+            <a href="" onclick="location.reload();">Home</a>
+          </li>
+          <li>
+            <a href="https://techiverse7.blogspot.com/" target="_blank">Blog</a>
+          </li>
+          <li>
+            <a href="https://github.com/mrz004" target="_blank">GitHub</a>
+          </li>
+        </ul>
+        <hr/>
+  `;
+} else {
+  document.getElementById("navbar").innerHTML = `
+  <li>
+  <a href="" onclick="location.reload();">Home</a>
+</li>
+<li>
+  <a href="https://techiverse7.blogspot.com/" target="_blank">Blog</a>
+</li>
+<li>
+  <a href="https://github.com/mrz004" target="_blank">GitHub</a>
+</li>
+<li>
+  <!-- <a href="codes.html" target="_blank">Codes</a> -->
+  <a id="codesButton">Codes</a>
+</li>
+  `;
+}
+document.getElementById("sidebar").style.left = "-100%";
+
+// function ToggleHide() {
+//   if (window.innerWidth < 768) {
+//     let searchbar = document.getElementById("navbar");
+//     let aro = document.getElementById("aro");
+
+//     if (searchbar.style.display == "none") {
+//       searchbar.style.display = "";
+//       aro.innerHTML = "ˆ";
+//     } else {
+//       searchbar.style.display = "none";
+//      aro.innerHTML = "ˇ";
+//     }
+//   }
 // }
 
-function toggleCodes() {
-  let codes = document.getElementById("sidebar");
-  if (codes.style.left != 0) {
-    codes.style.left = 0;
-  } else {
-    codes.style.left = -25;
-  }
-}
-
-if (window.innerWidth < 768) {
-  ToggleHide();
-}
-
-function ToggleHide() {
-  if (window.innerWidth < 768) {
-    let searchbar = document.getElementById("navdiv");
-    let aro = document.getElementById("aro");
-
-    if (searchbar.style.display == "none") {
-      searchbar.style.display = "";
-      aro.innerHTML = "ˆ";
-    } else {
-      searchbar.style.display = "none";
-      aro.innerHTML = "ˇ";
-    }
-  }
-}
+document.getElementById("webname").addEventListener("click", toggleCodes);
 
 // ToggleHide();
+
+let tcBool = false;
+function toggleCodes() {
+  // let code = document.getElementById("sidebar");
+  console.log(document.getElementById("sidebar").style.left);
+  if (tcBool == true) {
+    tcBool = false;
+    document.getElementById("sidebar").style.left = "-100%";
+    if (aro.innerHTML = "ˆ") {
+     aro.innerHTML = "ˇ";
+    }
+  } else {
+    tcBool = true;
+    document.getElementById("sidebar").style.left = "0%";
+    if (aro.innerHTML = "ˇ") {
+      aro.innerHTML = "ˆ";
+     }
+  }
+}
+
+document.getElementById("codesButton").addEventListener("click", toggleCodes);
