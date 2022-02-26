6340b0015e51a0cf3c0f64ecb1c6686c161c05a9
mr.z <mr.z004@yahoo.com>
2022-02-26 09:51:12 +0530

    made the layout littel better

--- script/script.js
+++ script/script.js
@@ -16,60 +16,45 @@ if (window.innerWidth < 769) {
         <hr/>
   `;
 } else {
+  aro.style.display = "none";
+
   document.getElementById("navbar").innerHTML = `
   <li>
   <a href="" onclick="location.reload();">Home</a>
-</li>
-<li>
+  </li>
+  <li>
   <a href="https://techiverse7.blogspot.com/" target="_blank">Blog</a>
-</li>
-<li>
+  </li>
+  <li>
   <a href="https://github.com/mrz004" target="_blank">GitHub</a>
-</li>
-<li>
+  </li>
+  <li>
   <!-- <a href="codes.html" target="_blank">Codes</a> -->
   <a id="codesButton">Codes</a>
-</li>
+  </li>
   `;
+
+  document.getElementById("codesButton").addEventListener("click", toggleCodes);
 }
 document.getElementById("sidebar").style.left = "-100%";
 
-// function ToggleHide() {
-//   if (window.innerWidth < 768) {
-//     let searchbar = document.getElementById("navbar");
-//     let aro = document.getElementById("aro");
-
-//     if (searchbar.style.display == "none") {
-//       searchbar.style.display = "";
-//       aro.innerHTML = "ˆ";
-//     } else {
-//       searchbar.style.display = "none";
-//      aro.innerHTML = "ˇ";
-//     }
-//   }
-// }
-
 document.getElementById("webname").addEventListener("click", toggleCodes);
-
-// ToggleHide();
 
 let tcBool = false;
 function toggleCodes() {
   // let code = document.getElementById("sidebar");
-  console.log(document.getElementById("sidebar").style.left);
+  // console.log(document.getElementById("sidebar").style.left);
   if (tcBool == true) {
     tcBool = false;
     document.getElementById("sidebar").style.left = "-100%";
-    if (aro.innerHTML = "ˆ") {
-     aro.innerHTML = "ˇ";
+    if ((aro.innerHTML = "ˆ")) {
+      aro.innerHTML = "ˇ";
     }
   } else {
     tcBool = true;
     document.getElementById("sidebar").style.left = "0%";
-    if (aro.innerHTML = "ˇ") {
+    if ((aro.innerHTML = "ˇ")) {
       aro.innerHTML = "ˆ";
-     }
+    }
   }
 }
-
-document.getElementById("codesButton").addEventListener("click", toggleCodes);
